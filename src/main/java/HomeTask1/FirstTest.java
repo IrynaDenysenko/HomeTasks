4183dccd4f69bf86b66bf77e0e047f895d99752d
Borys Denysenko <teabode@telenor.dk>
2023-05-02 00:36:34 -0700

    Fixing the issue

--- src/main/java/HomeTask1/FirstTest.java
+++ src/main/java/HomeTask1/FirstTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -20,7 +21,9 @@ public class FirstTest {
     private WebDriverWait wait;
     @BeforeTest
     public void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
        /* driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
         driver.manage().window().maximize();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(300));*/
