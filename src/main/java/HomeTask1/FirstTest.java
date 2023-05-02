73885033e502bd8f635138c03ee4b6c4129a4e7d
Iryna.Denysenko <iryna.denysenko@globallogic.com>
2023-05-02 10:22:01 +0300

    Please review code

--- src/main/java/HomeTask1/FirstTest.java
+++ src/main/java/HomeTask1/FirstTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.testng.annotations.AfterTest;
@@ -12,30 +13,56 @@ import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
 public class FirstTest {
     private WebDriver driver;
     private WebDriverWait wait;
     @BeforeTest
     public void setUp() {
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+       /* driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
         driver.manage().window().maximize();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(300));*/
     }
 
     @Test
     public void firstTest(){
-
-        driver.get("https://maportalstg.medavante.net/");
-
-        WebElement userName = driver.findElement(By.id("WebLogin_UserName"));
-        userName.sendKeys("v_adm2");
+        driver.get("https://google.com");
+        /*WebElement userName = driver.findElement(By.name("WebLogin$UserName"));
+        //wait.until(titleIs("MedAvante-ProPhase. A WIRB-Copernicus Group Company" ));
+        userName.sendKeys("v_adm2@dev-ad.test");
         WebElement password = driver.findElement(By.id("Password"));
-        password.sendKeys("Qwerty2@");
+        password.sendKeys("Qwerty2@#");
+        WebElement passwSwitcher = driver.findElement(By.id("passwSwitcher"));
+        passwSwitcher.click();
         WebElement login = driver.findElement(By.name("WebLogin$Login"));
         login.click();
-        WebElement Logo = driver.findElement(By.id("logo"));
+        boolean Logo = driver.findElement(By.id("logo")).isDisplayed();
+         */
     }
+
+    // private static ExpectedConditions getExpectedConditions() {
+    //    return ExpectedConditions;
+    //}
+
+   // @Test
+   // public void addUser(){
+    //    driver.get("https://maportalqa3.medavante.net/");
+     //   WebElement userName = driver.findElement(By.id("WebLogin_UserName"));
+    //  userName.sendKeys("v_adm1@dev-ad.test");
+    //    WebElement password = driver.findElement(By.id("Password"));
+    //    password.sendKeys("Qwerty1!");
+    //    WebElement passwSwitcher = driver.findElement(By.id("passwSwitcher"));
+     //   passwSwitcher.click();
+     //   driver.navigate().to("https://maportalqa3.medavante.net/raters/on-boarding");
+    //    WebElement addButton = driver.findElement(By.xpath("//div[@title='Add Site Staff'']"));
+     //   addButton.click();
+    //    wait.until(titleIs("Add Site Staff"));
+        //WebElement email = driver.findElement(By.cssSelector("input[type="email"]"));
+
+
+   // }
 
     @AfterTest
     public void tearDown() {
